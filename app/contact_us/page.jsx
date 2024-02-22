5d87acba74e9f3475b959df2086dd7777f18c7e0
ahmed sadekm <medo6medog@gmail.com>
2024-02-22 03:06:39 +0200

    i made the form in contact us page you can look and chang any thing you need to make it better

--- app/contact_us/page.jsx
+++ app/contact_us/page.jsx
@@ -1,10 +1,80 @@
+'use client'
 import React from 'react'
 import TitlePage from '../Components/main/titlePage'
+import "./contact.css"
+import { useState } from 'react';
 
-export default function ContactUs() {
+export default function Contactus() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [organization, setorganization] = useState('');
+  const [jobTitle, setJobTitle] = useState('');
+  const [phone, setphone] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+  };
   return (
     <>
       <TitlePage title={'Contact Us'} />
+      <section className='formsec'>
+        <form onSubmit={handleSubmit}>
+          <div className='div1'>
+            <div><label htmlFor="name">Name <span className='span'>*</span></label></div>
+            <input
+              type="text"
+              id="input"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+
+            <div><label htmlFor="email">Email <span className='span'>*</span></label></div>
+            <input
+              type="email"
+              id="input"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className='div2'>
+            <div><label htmlFor="organization">organization</label></div>
+            <input
+              type="text"
+              id="pers"
+              onChange={(e) => setorganization(e.target.value)}
+              required
+            />
+            <div><label htmlFor="job title">job title</label></div>
+            <input
+              type="text"
+              id="pers"
+              onChange={(e) => setJobTitle(e.target.value)}
+              required
+            />
+          </div>
+          <div className='div3'>
+            <div><label htmlFor="phone">phone</label></div>
+            <input
+              type="pers"
+              id="input"
+              onChange={(e) => setphone(e.target.value)}
+            />
+          </div>
+          <div className='div4'>
+            <div><label htmlFor="message">Message</label></div>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </section>
     </>
   )
 }
