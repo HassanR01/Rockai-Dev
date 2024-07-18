a2b2dec319ca6b2598e6b326731df264c2447277
hassanr01 <hassanrageh.236@gmail.com>
2024-07-18 21:22:48 +0300

    text edit

--- app/Components/projects/projectList.jsx
+++ app/Components/projects/projectList.jsx
@@ -37,7 +37,7 @@ export default async function ProjectList() {
                         <h3 className='font-bold'>{project.title}</h3>
                         {project.description.split(' ').length > 13 ? (<p title={project.description}>{project.description.split(" ").slice(0, 13).join(' ')}...</p>) : (<p>{project.description}.</p>)}
                         <div className="links">
-                            <Link className='demo' href={project.link} target='_blank'>visite</Link>
+                            <Link className='demo' href={project.link} target='_blank'>visit</Link>
                         </div>
                     </div>
                 </div>
